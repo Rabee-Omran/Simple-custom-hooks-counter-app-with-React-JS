8245242ec7e0bbc7605abcb7ad41f1c5b373f7f2
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-04-05 17:56:35 +0300

    initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+
+function App (){
+
+
+  
+
+     
+    return (
+      
+      <div>
+
+
+          <h1>TEST</h1>  
+
+
+      </div>
+    );
+ 
+}
+
+export default App;
