050f6e8f610dd2ba090fd2a6ed888b5ccfe4eee3
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-04-05 18:03:54 +0300

    simple counter with useState

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React,{useState} from 'react';
 function App (){
 
 
-  
+  const [count, setcount] = useState(0);
+
+
+  function increament(){
+    setcount(c => c + 1);
+
+  }
+
+  function decreament(){
+ 
+    setcount(c => c > 0 ? c - 1 : c)
+    
+  }
 
      
     return (
@@ -11,7 +23,9 @@ function App (){
       <div>
 
 
-          <h1>TEST</h1>  
+          <h1>Count : {count}</h1>  <br/> 
+          <button onClick={increament}>Increament</button>
+          <button onClick={decreament}>Decreament</button>
 
 
       </div>
